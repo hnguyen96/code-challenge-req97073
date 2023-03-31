08b0e85459cd1fe339d09ef802cca692df41cc79
Nam <hoangnamngx882@gmail.com>
2023-03-31 02:56:53 -0700

    Reorganized filter function

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -1,27 +1,58 @@
 import { Product } from "./Interface";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddProductForm from "./AddForm";
 import EditProductForm from "./EditForm";
 import { Table, Input, Label, FormGroup } from "reactstrap";
 
 
 // Taking a prop which is passed down from Homepage
-function ProductList(props: { data: any }){
+function ProductList(props: { data: any }) {
     const [searchName, setSearchName] = useState('');
     const [searchType, setSearchType] = useState('');
 
-    return(
+    const filterProducts = () => {
+        let filteredData = 
+            props.data
+            //Take data and filter it based on type and the name user is searching for
+            .filter((product: Product) => {
+                if (searchName !== "" && searchType == "scrum") {
+                    //Filter for scrumMaster
+                    let scrumName = product.scrumMasterName;
+
+                    if (scrumName.toLowerCase().includes(searchName.toLowerCase())) {
+                        return product
+                    }
+                } else if (searchName !== "" && searchType == "dev") {
+                    //Filter for developerS
+                    for (let i = 0; i < product.developers.length; i++) {
+                        if (product.developers[i].toLowerCase().includes(searchName.toLowerCase())) {
+                            return product;
+                        }
+                    }
+                } else {
+                    return product;
+                }
+            });
+        
+        return filteredData;
+    };         
+
+    return (
         <div>
             <h1 className="display-5 mb-3">Current active projects: {props.data.length}</h1>
             <div className="mx-auto w-75">
                 <FormGroup className="w-75 mx-auto mb-5">
-                    <div className="row align-items-end justify-content-between">  
-                        <div className="col-4">                    
+                    <div className="row align-items-end justify-content-between">
+                        <div className="col-4">
                             <Label for="searchType" className='mb-0 mt-2'>
                                 <span className='small text-white'>You are searching for..</span>
                             </Label>
-                            
-                            <Input id="searchType" name ="searchType" type="select" value={searchType} onChange={event => {setSearchType( event.target.value ); console.log(searchType)}}>
+
+                            <Input id="searchType" name="searchType" type="select" value={searchType} onChange={event => setSearchType(event.target.value)}>
+                                <option value="">
+                                    ...
+                                </option>
+
                                 <option value="scrum">
                                     Scrum Master
                                 </option>
@@ -30,14 +61,14 @@ function ProductList(props: { data: any }){
                                     Developer
                                 </option>
                             </Input>
-                        </div>  
+                        </div>
 
-                        <div className="col">  
+                        <div className="col">
                             <Label for="searchName" className='mb-0 mt-2'>
                                 <span className='small text-white'>Enter a name</span>
                             </Label>
 
-                            <Input id="searchName" name =" searchName" placeholder="Name.." required value={searchName} onChange={event => {setSearchName( event.target.value ); ; console.log(searchName)}}/>
+                            <Input id="searchName" name=" searchName" placeholder="Name.." required value={searchName} onChange={event => setSearchName(event.target.value)} />
                         </div>
 
                         <div className="col-auto">
@@ -61,45 +92,27 @@ function ProductList(props: { data: any }){
                     </thead>
 
                     <tbody>
-                        {props.data
-                        //Take data and filter it based on type and the name user is searching for
-                        .filter((product: Product) => {
-                            if(searchName !== "" && searchType =="scrum"){
-                                //Filter for scrumMaster
-                                let scrumName = product.scrumMasterName;
-                        
-                                if (scrumName.toLowerCase().includes(searchName.toLowerCase())){
-                                    return product
-                                } 
-                            } else if (searchName !== "" && searchType == "dev") {
-                                //Filter for developerS
-                                for( let i = 0; i < product.developers.length; i++){
-                                    if(product.developers[i].toLowerCase().includes(searchName.toLowerCase())){
-                                        return product;
-                                    }
-                                }
-                            } else {
-                                return product;
-                            }
-                        })
-                        .map((product: Product, index: number) => {
-                            return <tr key={product.productId}>
-                                <td className="col-2">{product.productId}</td>
-                                <td>{product.productName}</td>
-                                <td>{product.productOwnerName}</td>
-                                <td>
-                                    <ul>
-                                        {product.developers.map((dev) => (
-                                            <li key={dev}>{dev}</li>
-                                        ))}
-                                    </ul>
-                                </td>
-                                <td>{product.scrumMasterName}</td>
-                                <td>{product.startDate}</td>
-                                <td>{product.methodology}</td>
-                                <td><EditProductForm data = {product.productId} /></td>
-                            </tr>;
-                        })}
+                        {  
+                            filterProducts()
+                            .map((product: Product, index: number, filteredData: Product[]) => {
+                                return <tr key={product.productId}>
+                                    <td className="col-2">{product.productId}</td>
+                                    <td>{product.productName}</td>
+                                    <td>{product.productOwnerName}</td>
+                                    <td>
+                                        <ul>
+                                            {product.developers.map((dev) => (
+                                                <li key={dev}>{dev}</li>
+                                            ))}
+                                        </ul>
+                                    </td>
+                                    <td>{product.scrumMasterName}</td>
+                                    <td>{product.startDate}</td>
+                                    <td>{product.methodology}</td>
+                                    <td><EditProductForm data={product.productId} /></td>
+                                </tr>;
+                            })
+                        }
                     </tbody>
                 </Table>
             </div>
